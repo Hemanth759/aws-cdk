384cf07cdc2e5347f707d59412443b1667364b54
Rico Huijbers <rix0rrr@gmail.com>
2025-12-01 12:05:23 +0100

    Honk bonk

--- tools/@aws-cdk/cdk-build-tools/config/jest.config.js
+++ tools/@aws-cdk/cdk-build-tools/config/jest.config.js
@@ -17,11 +17,11 @@ module.exports = {
   testMatch: [`<rootDir>/test/**/?(*.)+(test).${ext}`],
 
   // Transform TypeScript using ts-jest. Use of this preset still requires the depending
-  // package to depend on `ts-jest` directly.
+  // package to depend on `ts-jest` directly. We need to use `babel-jest` on .js files to
+  // make sure `jest.mock` calls are hoisted to the top of every test file.
   transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest'
-    ],
+    "\\.jsx?$": ["babel-jest"],
+    '^.+\\.tsx?$': ['ts-jest'],
   },
   // Jest is resource greedy so this shouldn't be more than 50%
   maxWorkers: '50%',
